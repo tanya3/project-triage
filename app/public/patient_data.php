bc3ec3915ee6a10ff0eb42a2530c85f62226f82b
Tanya Gupta <tanya.gupta200@gmail.com>
2019-09-09 09:18:10 -0400

    Create patient_data.php

---
+++ app/public/patient_data.php
@@ -0,0 +1,27 @@
+[
+
+  {
+    "patientGuid":"P00001",
+    "firstName":"Tanya",
+    "lastName":"Gupta",
+    "dob":"1997-07-03",
+    "sexAtBirth":"F"
+  },
+
+  {
+    "patientGuid":"P00002",
+    "firstName":"Tony",
+    "lastName":"Stark",
+    "dob":"1970-12-09",
+    "sexAtBirth":"M"
+  },
+
+  {
+    "patientGuid":"P00003",
+    "firstName":"Natasha",
+    "lastName":"Romanoff",
+    "dob":"1982-11-01",
+    "sexAtBirth":"F"
+  },
+
+]
