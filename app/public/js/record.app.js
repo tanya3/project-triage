4fb389087585ddbc6beff4300068f945e7a6fbaf
Tanya Gupta <tanya.gupta200@gmail.com>
2019-09-18 09:47:42 -0400

    Class
    Classwork from Sept 18 2019. Added event listeners to form and table

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,23 +1,42 @@
 var patientRecordApp = new Vue({
-el: '#patientRecordApp',
-data: {
-    patients: []
-},
-methods: {
-  fetchPatients() {
-    fetch('patient_data.php')
-    .then(response => response.json())          //creating a separate function that calls reponse.json() and stores it in json
-    .then( json => {patientRecordApp.patients = json });   //creating another function
+  el: '#patientRecordApp',
+  data: {
+      patients: [],
+      formPatient: {
+        firstName: '',
+        lastName: '',
+        dob: '',
+        sexAtBirth: ''
+      }
+  },
+  methods: {
+    fetchPatients() {
+      fetch('patient_data.php')
+      .then(response => response.json())          //creating a separate function that calls reponse.json() and stores it in json
+      .then( json => {patientRecordApp.patients = json });   //creating another function
 
+    },
 
-    // fetch('https://randomuser.me/api/')
-    // .then(function(response) {return response.json()})          //creating a separate function that calls reponse.json() and stores it in json
-    // .then(function(json) {waitingApp.people=json});
-  }
-},
+    handleCreateRecord(event) {
+      //TODO: POST the form content somewhere
+      //fetch(url, {method:"POST", data:this.formPatient})
+      //.then(...)
+      this.patients.push(this.formPatient);
+      this.formPatient = {
+        firstName: '',
+        lastName: '',
+        dob: '',
+        sexAtBirth: ''
+      }
+    },
 
-created() {                                    //called automatically when new application is created
-   this.fetchPatients();
- }
+    selectRecord(patient) {
+      console.log(patient);
+    }
+  },
+
+  created() {                                    //called automatically when new application is created
+     this.fetchPatients();
+   }
 
 });
--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,23 +1,23 @@
 var patientWaitingApp = new Vue({
-el: '#patientWaitingApp',
-data: {
-    patients: []
-},
-methods: {
-  fetchPatients() {
-    fetch('patient_data.php')
-    .then(response => response.json())          //creating a separate function that calls reponse.json() and stores it in json
-    .then( json => {patientWaitingApp.patients = json});   //creating another function
+  el: '#patientWaitingApp',
+  data: {
+      patients: []
+  },
+  methods: {
+    fetchPatients() {
+      fetch('patient_data.php')
+      .then(response => response.json())          //creating a separate function that calls reponse.json() and stores it in json
+      .then( json => {patientWaitingApp.patients = json});   //creating another function
 
 
-    // fetch('https://randomuser.me/api/')
-    // .then(function(response) {return response.json()})          //creating a separate function that calls reponse.json() and stores it in json
-    // .then(function(json) {waitingApp.people=json});
+      // fetch('https://randomuser.me/api/')
+      // .then(function(response) {return response.json()})          //creating a separate function that calls reponse.json() and stores it in json
+      // .then(function(json) {waitingApp.people=json});
+    }
+  },
+
+  created() {                                    //called automatically when new application is created
+     this.fetchPatients();
   }
-},
-
-created() {                                    //called automatically when new application is created
-   this.fetchPatients();
- }
 
 });
