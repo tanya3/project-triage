3e2e61a89b3f919340a06e2272bee6cb079cbb2c
Tanya Gupta <tanya.gupta200@gmail.com>
2019-10-16 10:31:32 -0400

    bug fix

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -9,22 +9,12 @@ var waitingApp = new Vue({
       .then(response => response.json())
       .then(json => { waitingApp.patients = json })
     },
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-    formatVisitDateLocal(d) {
-      return moment.utc(d).local().format("HH:mm MMM Do");
-=======
->>>>>>> pr/6
     formatVisitDateLocal(d) {
       return moment.utc(d).local().format("HH:mm MMM Do");
     },
     visitDateSince(d) {
       return moment.utc(d).local().fromNow();
-<<<<<<< HEAD
->>>>>>> pr/6
-=======
->>>>>>> pr/6
     }
   },
   created() {
