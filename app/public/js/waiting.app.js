4584bf7fef966005767c12a567fd93e799dd5115
Tanya Gupta <tanya.gupta200@gmail.com>
2019-10-16 10:29:05 -0400

    class

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -10,15 +10,21 @@ var waitingApp = new Vue({
       .then(json => { waitingApp.patients = json })
     },
 <<<<<<< HEAD
+<<<<<<< HEAD
 
     formatVisitDateLocal(d) {
       return moment.utc(d).local().format("HH:mm MMM Do");
 =======
+=======
+>>>>>>> pr/6
     formatVisitDateLocal(d) {
       return moment.utc(d).local().format("HH:mm MMM Do");
     },
     visitDateSince(d) {
       return moment.utc(d).local().fromNow();
+<<<<<<< HEAD
+>>>>>>> pr/6
+=======
 >>>>>>> pr/6
     }
   },
